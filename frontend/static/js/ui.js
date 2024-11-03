0db77a16e1538be7c3a281a2dffc0dee842683ec
mmdavid1 <96997314+mmdavid1@users.noreply.github.com>
2024-11-03 10:53:09 -0800

    started react app

--- frontend/static/js/ui.js
+++
@@ -1,6 +0,0 @@
-export function updateUI(message) {
-    const apiResponse = document.getElementById("apiResponse");
-    if (apiResponse) {
-        apiResponse.innerText = message;
-    }
-}
--- frontend/static/ts/main.ts
+++
@@ -1,7 +0,0 @@
-import { fetchMessage } from "./api.js";
-import { updateUI } from "./ui.js";
-
-document.getElementById("apiButton")?.addEventListener("click", async () => {
-    const message = await fetchMessage();
-    updateUI(message);
-});
