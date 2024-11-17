831c984f4f883610671aea07e996de165a3eb07d
Anusha Chadalavada <chad.anusha@gmail.com>
2024-11-17 10:45:27 -0800

    front end

--- frontend/src/components/Square.tsx
+++ frontend/src/components/Square.tsx
@@ -1,8 +1,30 @@
 import React from "react"
-import Piece from "./Piece"
+import Piece from "../Piece"
+import './Square.css';
 
-export default function square(){
-    return (
-        <p>hi</p>
-    )
+interface Props{
+    image?: string;
+    number: number
 }
+
+export default function Square({number, image}: Props){
+    if (number % 2 ===0){
+        return (
+            <div className = "tile black-tile" >
+                {image && <div style = {{backgroundImage: `url(${image})`}} className = "chess-piece"></div>}
+            </div>
+        );
+    }else{
+        return(
+            <div className = "tile white-tile" >
+                {image &&<div style = {{backgroundImage: `url(${image})`}} className = "chess-piece"></div>}
+            </div>
+        );
+    }
+    
+
+    
+}
+
+
+
