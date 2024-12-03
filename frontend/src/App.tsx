47527b36e17329464a9aa5375897974f8904df9f
mmdavid1 <96997314+mmdavid1@users.noreply.github.com>
2024-12-03 11:36:19 -0800

    fixed front-end routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,16 +2,26 @@ import React from 'react'
 import './App.css'
 import Board from './components/Board';
 import Abandon from './components/Abandon';
-import { BrowserRouter } from 'react-router';
+import StartPage from './pages/StartPage';
+import AbandonPage from './pages/AbandonPage';
+import { BrowserRouter, Routes, Route } from 'react-router';
 
 function App() {
   return (
     <BrowserRouter>
-      <Board/>
-      <Abandon/>
+      <Routes>
+        <Route path="/" element={<StartPage />} />
+        <Route path="/game" element={
+          <>
+            <Board />
+            <Abandon />
+          </>
+        } />
+       <Route path="/abandon" element={<AbandonPage />} />
+
+      </Routes>
     </BrowserRouter>
   )
-
 }
 
 export default App;
